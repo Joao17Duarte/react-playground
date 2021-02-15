7c2b74a3fcda93ea4b386f95553e409354454583
Joao Duarte <joaopd17@hotmail.com>
2021-02-15 16:39:34 +0100

    Update styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,22 +6,24 @@ import './App.css'
 export default function App({ question, answer, radius }) {
   return (
     <>
-      <AppHeader title="React Intro" />
-      <Card
-        radius="20"
-        question="Do you like React?"
-        answer="Yes, it's very Cool"
-      />
-      <Card
-        radius="20"
-        question="Do you like JS?"
-        answer="No, it's very hard"
-      />
-      <Card
-        radius="20"
-        question="Do you like Hamburg?"
-        answer="Yes, it's a very nice City but really cold "
-      />
+      <div className="App">
+        <AppHeader title="React Intro" />
+        <Card
+          radius="20"
+          question="Do you like React?"
+          answer="Yes, it's very Cool"
+        />
+        <Card
+          radius="20"
+          question="Do you like JS?"
+          answer="No, it's very hard"
+        />
+        <Card
+          radius="20"
+          question="Do you like Hamburg?"
+          answer="Yes, it's a very nice City but really cold "
+        />
+      </div>
     </>
   )
 }
